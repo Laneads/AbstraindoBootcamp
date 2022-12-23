44edb96bc60952c75361234ff9688d309713e63a
Laneads <conversecomlane@gmail.com>
2022-12-22 22:02:38 -0300

    começo

---
+++ DesafioPOO/src/br/com/dio/desafio/dominio/Cursos.java
@@ -0,0 +1,27 @@
+package br.com.dio.desafio.dominio;
+
+public class Cursos {
+    String titulo;
+    String descricao;
+    int cargaHoraria;
+
+    
+    public String getTitulo() {
+        return titulo;
+    }
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+    public String getDescricao() {
+        return descricao;
+    }
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+}
